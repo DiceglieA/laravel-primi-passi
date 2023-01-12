d0162c7e3c9bd2e2d1dc5c54a0d6775987699c69
DiceglieA <angelodiceglie10@gmail.com>
2023-01-12 15:30:36 +0100

    last commit func compact e dinamic in route /ciao

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<?php
+$title = 'Hello World';
+?>
+
+{{-- metodo tradizionale visto in classe --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+</head>
+<body>
+    <h1>HELLO WORLD</h1>
+
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">home</a>
+                </li>
+                <li>
+                    <a href="/ciao">ciao</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // $title = 'Hello World';
+    return view('home');
+});
+
+Route::get('/ciao', function () {
+    $title = 'Hello World';
+    return view('ciao', compact('title')); //utilizzata funzione compact
 });
